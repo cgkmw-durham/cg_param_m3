db591d10c433a437d709ef5ff78c68567969befe
POTTER <ngrz58@login1.ham8.dur.ac.uk>
2022-02-22 13:01:02 +0000

    Added amine-oxide fragments to list

--- cg_param_m3.py
+++ cg_param_m3.py
@@ -1219,7 +1219,7 @@ def get_smarts_matches(mol):
     #Get matches to SMARTS strings
     smarts_strings = {
     #'S([O-])(=O)(=O)O'  :    'Q4',
-    'S([O-])(=O)(=O)'   :    'Q4'#SQ5
+    'S([O-])(=O)(=O)'   :    'Q4'#SQ4
     #'C(=O)O' : 'P1'#SP1
     #'CC' : 'C2',
     #'OO' : 'P5'
