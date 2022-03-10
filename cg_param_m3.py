b32f21c627693eba5d4386305e5245eb62b9ad2b
POTTER <ngrz58@login1.ham8.dur.ac.uk>
2022-03-10 14:20:50 +0000

    Contains all fixes, but tuning turned off for now

--- cg_param_m3.py
+++ cg_param_m3.py
@@ -1303,7 +1303,7 @@ A_cg,beads,ring_beads,path_matrix = mapping(mol,ring_atoms,matched_maps,3)
 non_ring = [b for b in range(len(beads)) if not any(b in ring for ring in ring_beads)]
 
 #Parametrise beads
-tuning = True
+tuning = False
 bead_types,charges,all_smi,DG_data = get_types(beads,mol,ring_beads)
 
 #Generate atomistic conformers
